904480be16d576288d89a48d39f086bf2cc4b43b
imgdrive <support@yubsoft.com>
2024-06-05 20:06:44 +0800

    First commit

---
+++ ENU.h
@@ -0,0 +1,28 @@
+﻿{0x0409, L"English", {
+    L"&Source:",
+    L"&Target:",
+    L"&Label:",
+    L"&File system:",
+    L"&Start",
+    
+    L"Select or drag folder here",
+    L"Select output ISO file",
+    L"0 to 128 characters of label",
+    
+    L"ISO9660",
+    L"ISO9660 + UDF 1.02 (DVD)",
+    L"UDF 1.02",
+    L"UDF 2.50 (BD/UHD)",
+    
+    L"&Language",
+    L"&Integrate into shell context menu",
+    L"&Close when done",
+    L"&Stay on top",
+    L"&Translation",
+    L"&Report issues",
+    L"&About Folder2ISO...",
+    
+    L"'%s' already exists.\n\nDo you want to replace it?",
+    L"ISO files (*.iso)\0*.iso\0\0",
+    L"This program is freeware.\n\nAuthor: Young, Fred, Kira\nTranslator: null",
+}},
