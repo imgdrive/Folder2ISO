5b4a3e0b6353f6759c3b542ec62784030b96bdc4
imgdrive <support@yubsoft.com>
2024-06-10 13:26:28 +0800

    Add CHT/DEU/ITA translations

---
+++ DEU.h
@@ -0,0 +1,29 @@
+﻿{0x0407, L"German (Deutsch)", {
+    L"Quelle:",
+    L"Ziel:",
+    L"Label:",
+    L"Dateisystem:",
+    L"Starten",
+    
+    L"Wählen Sie einen Ordner aus oder ziehen Sie diesen hierher",
+    L"Wählen Sie die Ausgabe-ISO-Datei aus",
+    L"0 bis 128 Zeichen des Volume-Label",
+    
+    L"ISO9660",
+    L"ISO9660 + UDF 1.02 (DVD)",
+    L"UDF 1.02",
+    L"UDF 2.50 (BD/UHD)",
+    
+    L"&Sprache (Language)",
+    L"In Shell-Kontextmenü &integrieren",
+    L"Nach Abschluss &schließen",
+    L"&Immer im Vordergrund",
+    L"&Translation",
+    L"Probleme &melden",
+    L"&Befehlszeile",
+    L"&Über Folder2ISO ...",
+    
+    L"'%s' ist bereits vorhanden.\n\nMöchten Sie diese ersetzen?",
+    L"ISO-Dateien (*.iso)\0*.iso\0\0",
+    L"Dieses Programm ist Freeware.\n\nAutor: Young, Fred, Kira\nTranslator: null",
+}},
