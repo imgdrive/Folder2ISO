257ffeeb18e699b827c4c4333a5d3d9147fa4ae2
imgdrive <support@yubsoft.com>
2024-06-13 21:55:34 +0800

    Add Czech translation

---
+++ CSY.h
@@ -0,0 +1,36 @@
+﻿{
+    L"1.2",         // Verze
+    0x0409,         // 0405
+    L"Czech (čeština)",     // CSY
+    {
+        L"Zdroj:",
+        L"Cílová složka:",
+        L"Označení:",
+        L"Souborový systém:",
+        L"Start",
+        
+        L"Zde vyberte nebo přetáhněte složku",
+        L"Vyberte výstupní soubor ISO",
+        L"0 až 128 znaků jmenovky svazku",
+        
+        L"ISO9660",
+        L"ISO9660 + UDF 1.02 (DVD)",
+        L"UDF 1.02",
+        L"UDF 2.50 (BD/UHD)",
+        
+        L"&Jazyk",
+        L"&Integrace do kontextové nabídky",
+        L"&Povolit více instancí",
+        L"&Po dokončení zobrazit v Průzkumníku",
+        L"&Po dokončení uzavřít",
+        L"&Zůstane zobrazeno",
+        L"&Překlad",
+        L"&Nahlásit problémy",
+        L"&Příkazový řádek",
+        L"&O programu Folder2ISO...",
+        
+        L"'%s' již existuje.\n\nChcete jej nahradit?",
+        L"ISO soubory (*.iso)\0*.iso\0\0",
+        L"Tento program je freeware.\n\nAutor: Young, Fred, Kira\nTranslator: hifi.pepino", // Nahraďte null jménem překladatele
+    }
+},
