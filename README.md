b3d024a4159b45b8aab26e71e0e9a806c72e65e4
imgdrive <support@yubsoft.com>
2024-11-18 07:44:27 +0800

    Add Slovak translation

--- README.md
+++ README.md
@@ -19,6 +19,7 @@ Folder2ISO is a free portable standalone tool to create an ISO Image from a fold
     <tr><td>Polish (Polski)         </td><td><a href="PLK.h">CHS.h</a></td><td>&#x1F34F; 1.2</td><td>xesarni</td></tr>
     <tr><td>Portuguese (Brazil)     </td><td><a href="PTB.h">PTB.h</a></td><td>&#x1F34F; 1.2</td><td>igorruckert</td></tr>
     <tr><td>Russian (Русский)       </td><td><a href="RUS.h">RUS.h</a></td><td>&#x1F34F; 1.2</td><td>jekovcar</td></tr>
+    <tr><td>Slovak (Slovenčina)     </td><td><a href="SKY.h">SKY.h</a></td><td>&#x1F34F; 1.2</td><td>jekovcar</td></tr>
     <tr><td>Turkish (Türkçe)        </td><td><a href="TRK.h">TRK.h</a></td><td>&#x1F34F; 1.2</td><td>Yaşar Bulut</td></tr>
 </table>
 
---
+++ SKY.h
@@ -0,0 +1,41 @@
+﻿{
+    L"1.4",         // Verzia
+    0x041b,         // 041b
+    L"Slovak (Slovenčina)",     // SKY
+    {
+        L"Zdroj:",
+        L"Cieľ:",
+        L"Menovka:",
+        L"Systém súborov:",
+        L"Štart >>",
+        
+        L"Tu vyberte alebo pretiahnite priečinok",
+        L"Tu zadajte názov výstupného súboru ISO",
+        L"Menovka (0 až 128 znakov)",
+        
+        L"ISO9660",
+        L"ISO9660 + UDF 1.02 (DVD)",
+        L"UDF 1.02",
+        L"UDF 2.50 (BD/UHD)",
+        
+        L"&Jazyk",
+        L"&Integrovať do kontextového menu",
+        L"&Povoliť viac inštancií",
+        L"Po &dokončení otvoriť v Prieskumníkovi",
+        L"Po dokončení &ukončiť program",
+        L"O&kno vždy navrchu",
+        L"Ja&zykový preklad",
+        L"&Nahlásiť problémy",
+        L"&Príkazový riadok",
+        L"&O programe Folder2ISO...",
+        
+        L"&Téma",
+        L"&Svetlá",
+        L"T&mavá",
+        L"&Podľa systému",
+        
+        L"'%s' už existuje.\n\nChcete ho nahradiť?",
+        L"ISO súbory (*.iso)\0*.iso\0\0",
+        L"Tento program je zdarma.\n\nAutor: Young, Fred, Kira\nPreložil: Rudolfin",
+    }
+},
