a9dfa52a097f30c6a00176ecc2e9d915c14142f2
Lieba Natur Brilian <gingalibadeidara@gmail.com>
2025-01-17 18:11:18 +0700

    Add Indonesian Translation

---
+++ IND.h
@@ -0,0 +1,41 @@
+{
+    L"1.3",         // Version
+    0x0421,         // LCID - Locale identifier
+    L"Indonesian (ID)",     // Language name
+    {
+        L"Sumber:",
+        L"Tujuan:",
+        L"Label:",
+        L"Berkas sistem:",
+        L"Mulai",
+        
+        L"Pilih atau seret folder disini",
+        L"Pilih berkas ISO keluaran",
+        L"0 dari 128 karakter label volume",
+        
+        L"ISO9660",
+        L"ISO9660 + UDF 1.02 (DVD)",
+        L"UDF 1.02",
+        L"UDF 2.50 (BD/UHD)",
+        
+        L"&Bahasa",
+        L"&Integrasikan ke menu konteks shell",
+        L"&Izinkan instance multiple",
+        L"&Tampilkan di Explorer ketika sudah selesai",
+        L"&Tutup ketika sudah selesai",
+        L"&Tetap di atas",
+        L"&Diterjemahkan",
+        L"&Laporkan isu",
+        L"&Baris perintah",
+        L"&Tentang Folder2ISO...",
+        
+        L"&Tema",
+        L"&Terang",
+        L"&Gelap",
+        L"&Sistem",
+        
+        L"'%s' sudah ada.\n\nApakah anda ingin menimpanya?",
+        L"Berkas ISO (*.iso)\0*.iso\0\0",
+        L"Program ini gratis.\n\nDibuat: Young, Fred, Kira\nPenerjemah: Lieba Natur Brilian", // Replace null with the translator name
+    }
+},
