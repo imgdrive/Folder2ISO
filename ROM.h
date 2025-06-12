06c2b5d5f8c4856da1e06df47a5f1f6013beb32c
Madalin <72721570+C0M9H@users.noreply.github.com>
2025-06-12 07:42:51 +0300

    Create ROM.h

---
+++ ROM.h
@@ -0,0 +1,41 @@
+{
+    L"1.3",         // Version
+    0x0418,         // LCID - Locale identifier
+    L"Romanian",     // Language name
+    {
+        L"Sursa:",
+        L"Destinatie:",
+        L"Titlu:",
+        L"Fiser System:",
+        L"Incepe",
+        
+        L"Selecteaza sau trage un folder aici!",
+        L"Selecteaza fiserul de iesire!",
+        L"0 to 128 charactere maxim",
+        
+        L"ISO9660",
+        L"ISO9660 + UDF 1.02 (DVD)",
+        L"UDF 1.02",
+        L"UDF 2.50 (BD/UHD)",
+        
+        L"&Limba",
+        L"&Adauga la context menu",
+        L"&Permite multiple instanțe",
+        L"&Afiseaza locatia in Explorer",
+        L"&Inchide dupa creare",
+        L"&Tine la suprafata",
+        L"&Traduceri",
+        L"&Raporteaza o eroare",
+        L"&Linie de comanda",
+        L"&Despre Folder2ISO...",
+        
+        L"&Tema",
+        L"&Luminoasa",
+        L"&Intunecata",
+        L"&System",
+        
+        L"'%s' Deja exista.\n\nVrei sa inlocuiesti?",
+        L"ISO files (*.iso)\0*.iso\0\0",
+        L"Acest program este gratuit.\n\nAuthor: Young, Fred, Kira\nTranslator: CMH09", // Replace null with the translator name
+    }
+},
