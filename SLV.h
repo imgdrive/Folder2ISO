78dc984b54c2d68755798cf131a9a15b6861b81c
max-ima <96241406+max-ima@users.noreply.github.com>
2025-06-20 09:54:25 +0200

    Slovenian translation
    Slovenian translation

---
+++ SLV.h
@@ -0,0 +1,41 @@
+﻿{
+    L"1.4.3",         // Version
+    0x0424,         // LCID - Locale identifier
+    L"Slovenian",     // Language name
+    {
+        L"Vir:",
+        L"Cilj:",
+        L"Label:",
+        L"Datotečni sistem:",
+        L"Začni",
+        
+        L"Izberite ali povlecite mapo sem",
+        L"Izberite izhodno datoteko ISO",
+        L"0 do 128 znakov oznake nosilca",
+        
+        L"ISO9660",
+        L"ISO9660 + UDF 1.02 (DVD)",
+        L"UDF 1.02",
+        L"UDF 2.50 (BD/UHD)",
+        
+        L"&Slovenian",
+        L"&Integriraj v kontekstni meni lupine",
+        L"&Dovoli več primerkov",
+        L"&Po končanem delu razkrij v Raziskovalcu",
+        L"&Zapri, ko končaš",
+        L"&Ostani na vrhu",
+        L"&Prevod",
+        L"&Prijavi težave",
+        L"&Ukazna vrstica",
+        L"&O Folder2ISO...",
+        
+        L"&Tema",
+        L"&Svetla",
+        L"&Temna",
+        L"&Sistem",
+        
+        L"'%s' že obstaja.\n\nAli želite zamenjati?",
+        L"ISO datoteke (*.iso)\0*.iso\0\0",
+        L"Ta program je brezplačen..\n\nAvtor: Young, Fred, Kira\nTranslator: Marko(max)", // Replace null with the translator name
+    }
+},
